3f751447292807aae75b1be4cb72d4a2130530c8
Harry <wang.haoda@protonmail.com>
2017-09-25 23:44:23 +0000

    add more to the djikstra

--- slider.py
+++ slider.py
@@ -1,5 +1,3 @@
-import Queue
-
 def manhattan(r, c, puzzle):
     target_row = puzzle[r][c] / 4
     target_col = puzzle[r][c] % 4
@@ -15,5 +13,42 @@ def total_cost(puzzle):
 
     return total
 
-def djikstra(puzzle):
-    return "todo"
+def find_space(puzzle):
+    for row, col in enumerate(puzzle):
+        if -1 in col:
+            return [row, col]
+
+def djikstra(puzzle, prev_cost):
+    # Get the current cost of this iteration
+    curr_cost = total_cost(puzzle)
+    print(curr_cost)
+
+    # If it has a greater cost, turn back
+    if curr_cost < prev_cost:
+        return False
+
+    # Now try out each new move
+    space = find_space(puzzle)
+    # Do it up-down
+    for i in [-1, 1]:
+        puzz = puzzle
+        # swap out the space and the thing to be changed
+        temp = puzz[space[0]][space[1]]
+        puzz[space[0]][space[1]] = puzz[space[0] + i][space[1]]
+        puzz[space[0] + i][space[1]] = temp
+
+        if djikstra(puzz, curr_cost):
+            print("uau")
+            exit(0)
+
+    # Now do it left-right
+    for i in [-1, 1]:
+        puzz = puzzle
+        # swap out the space and the thing to be changed
+        temp = puzz[space[0]][space[1]]
+        puzz[space[0]][space[1]] = puzz[space[0]][space[1] + i]
+        puzz[space[0]][space[1] + i] = temp
+
+        if djikstra(puzz, curr_cost):
+            print("uau")
+            exit(0)
