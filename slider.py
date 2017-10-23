0d9d07f3380e48d42544bb82083a32dc6366b64d
Harry <wang.haoda@protonmail.com>
2017-10-23 15:42:09 -0400

    i finished

--- slider.py
+++ slider.py
@@ -1,10 +1,15 @@
 import re
 import sys
 import hashlib
+import copy
 
+# This thing stores md5 sums of states visited in the past
 puzz_list = []
+# "I will take The Ring," he said, "though i do not know the way."
+stack = []
 
 
+# Calculates the manhattan distance of one square
 def manhattan(r, c, puzzle):
     target_row = puzzle[r][c] // 4
     target_col = puzzle[r][c] % 4
@@ -13,6 +18,7 @@ def manhattan(r, c, puzzle):
     return d
 
 
+# Checks the total manhattan distance of one state
 def total_cost(puzzle):
     total = 0
     for i in range(0, 4):
@@ -22,6 +28,7 @@ def total_cost(puzzle):
     return total
 
 
+# Finds the indices of the square that's the space
 def find_space(puzzle):
     for i in range(0, 4):
         for it in range(0, 4):
@@ -29,11 +36,13 @@ def find_space(puzzle):
                 return [i, it]
 
 
+# Pure magic
 def djikstra(puzzle, prev_cost):
     # Get the current cost of this iteration
     curr_cost = total_cost(puzzle)
 
     # If this array has already been checked, turn back
+    # Using md5 sums to save space
     m = hashlib.md5()
     m.update(str(puzzle).encode('utf-8'))
     md5 = m.hexdigest()
@@ -42,12 +51,9 @@ def djikstra(puzzle, prev_cost):
     else:
         puzz_list.append(md5)
 
-    for i in puzzle:
-        print(i)
-    print(curr_cost)
-
     # If it has zero cost, done
     if curr_cost is 0:
+        stack.append(puzzle)
         return True
     elif curr_cost > prev_cost:
         return False
@@ -56,47 +62,58 @@ def djikstra(puzzle, prev_cost):
     # Do it up-down
     for i in [-1, 1]:
         space = find_space(puzzle)
-        puzz = puzzle[:]
+        puzz = copy.deepcopy(puzzle)
         if 0 <= space[0] + i <= 3:
             # swap out the space and the thing to be changed
             temp = puzz[space[0] + i][space[1]]
             puzz[space[0] + i][space[1]] = puzz[space[0]][space[1]]
             puzz[space[0]][space[1]] = temp
 
-            if djikstra(puzz, curr_cost):
+            # This way got to the right solution so add this state onto the thing
+            if djikstra(puzz, curr_cost) is True:
+                stack.append(puzzle)
                 return True
 
     # Now do it left-right
     for i in [-1, 1]:
         space = find_space(puzzle)
-        puzz = puzzle[:]
+        puzz = copy.deepcopy(puzzle)
         if 0 <= space[1] + i <= 3:
             # swap out the space and the thing to be changed
             temp = puzz[space[0]][space[1] + i]
             puzz[space[0]][space[1] + i] = puzz[space[0]][space[1]]
             puzz[space[0]][space[1]] = temp
 
-            if djikstra(puzz, curr_cost):
+            # This way got to the right solution so add this state onto the thing
+            if djikstra(puzz, curr_cost) is True:
+                stack.append(puzzle)
                 return True
 
     return False
 
 
 # Read the file and put it into an array
-f = open("puzzle.txt", "r")
+f = open('puzzle.txt', 'r')
 inp = f.readlines()
 puz = []
 for string in inp:
     puz.append(re.findall("[\w']+", string))
 
+# Turn letters into numbers for easier manhattan distance things
 for i in range(0, 4):
     for it in range(0, 4):
-        puz[i][it] = ord(puz[i][it]) - 65
         # make sure to turn S into #15
-        if puz[i][it] is 18:
-            puz[i][it] = 15
+        puz[i][it] = 15 if ord(puz[i][it]) - 65 is 18 else ord(puz[i][it]) - 65
 
-print(puz)
+# Actually do the thing
+djikstra(puz, sys.maxsize)
 
-print(djikstra(puz, sys.maxsize))
-print(puzz_list)
+# Print the finished thing
+for puzzle in reversed(stack):
+    for row in puzzle:
+        temp = ''
+        for square in row:
+            # m a g i c
+            temp += chr(83 if square == 15 else square + 65) + ' '
+        print(temp)
+    print()
