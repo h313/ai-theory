3d5fb75976a20fc7abc6b2392fba82bf9c16d923
Harry <harry@h313.info>
2017-11-16 09:57:01 -0500

    did things

--- simple_feedforward.cpp
+++ simple_feedforward.cpp
@@ -22,16 +22,11 @@ public:
         if (weights.size() == layer_output -> size()) {
             // get the z thing by adding each connecting neuron up
             float z = 0;
-            for(int i = 0; i < weights.size(); i++)
-                cout << weights[i] << " ";
-            for(int i = 0; i < layer_output -> size(); i++)
-                cout << layer_output -> at(i) << " ";
-            cout << endl << bias << endl;
-            for(int input_index = 0; input_index < 0; input_index++) {
+			for(int input_index = 0; input_index < weights.size(); input_index++) {
                 z += float(weights[input_index]) * float(layer_output -> at(input_index)) - bias;
             }
+            cout << z << endl;
             // return the sigmoid function
-            cout << "z is " << z << endl;
             return 1.0 / (1.0 - exp(-z));
         }
     }
@@ -40,7 +35,7 @@ public:
     Neuron(int Bias, vector<float> Weights) : weights(Weights) {
         bias = Bias;
     }
-    // Override for droupout neurons (using bias for multiple things now!)
+    // Override for dropout neurons (using bias for multiple things now!)
     Neuron(bool Const_Output, int Bias) {
         bias = Bias;
         const_output = Const_Output;
@@ -59,9 +54,6 @@ public:
         vector<float> ret = {};
         // Get the outputs of the above layers recursively
         vector<float> inputs = layer_above -> get_layer_output();
-		cout << "inputs" << endl;
-		for(int i = 0; i < inputs.size(); i++)
-			cout << inputs[i] << endl;
         // Calculates the sigmoid of each input
         for(int i = 0; i < neurons.size(); i++) {
             ret.push_back(neurons[i] -> sigmoid(&inputs));
@@ -94,14 +86,14 @@ public:
 int main(){
     // Declare the neurons in the middle layer
     vector<float> h1_weights = {5.0, 6.0, -3.0};
-    Neuron h1(1, h1_weights);
+    Neuron h1(0.5, h1_weights);
     vector<float> h2_weights = {-6.0, 6.0, 3.0};
-    Neuron h2(1, h2_weights);
-    Neuron b2(true, 1);
+    Neuron h2(0.5, h2_weights);
+    Neuron b2(true, 1.0);
 
     // Declare the neuron in the output layer
     vector<float> o1_weights = {10.0, 10.0, -5.0};
-    Neuron o1(1, o1_weights);
+    Neuron o1(0.5, o1_weights);
 
     cout << "Neurons created!" << endl;
 
@@ -121,7 +113,7 @@ int main(){
 
     // Get the output of the layer
     vector<float> output = output_layer.get_layer_output();
-    cout << output[0] << endl;
+    cout << "output is " << output[0] << endl;
 
     return 0;
 }
