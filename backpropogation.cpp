250c930da5ca3503c54acf0b2777910a99b20227
Harry <harry@h313.info>
2017-12-29 13:05:50 -0500

    Update backpropogation.cpp

--- backpropogation.cpp
+++ backpropogation.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include <cmath>
 
-#define LEARN_WEIGHT 0.7
-#define MOMENTUM 0.3
+#define LEARN_RATE 0.7f
+#define MOMENTUM 0.3f
 
 using namespace std;
 
-// makes a random float from -10.0 to 10.0 to work with building the 
+// Makes a random float from -10.0 to 10.0
 float rand_float() {
 	return -10.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 20.0f));
 }
@@ -35,29 +35,30 @@ public:
 	// Sigmoid function
 	float sigmoid(vector<float>* layer_output) {
 		// If it's a dropout neuron, just return the bias
-		if (const_output)
+		if (this -> const_output)
 			return bias;
 		// only works if layer_output is the same length as weights
-		if (weights.size() == layer_output->size()) {
+		else if (weights.size() == layer_output -> size()) {
 			// get the z thing by adding each connecting neuron up
 			float z = 0;
 			for (int input_index = 0; input_index < weights.size(); input_index++) {
-				z += float(weights[input_index]) * float(layer_output->at(input_index)) - bias;
+				z += float(weights[input_index]) * float(layer_output -> at(input_index)) - bias;
 			}
 			// return the sigmoid function
-			return 1.0 / (1.0 + exp(-z));
+			return 1.0f / (1.0f + exp(-z));
 		}
 	}
 	// Backpropogation for just this neuron
 	void backpropogate(float error, vector<float>* layer_output) {
 		// Only do it if it's not a dropout neuron
 		if (!this -> const_output) {
-			for (int i = 0; i < this->weights.size(); i++) {
-				// Put the previous weight into the previous vector
-				float temp = this->previous[i];
-				this -> previous[i] = this -> weights[i];
+			for (int i = 0; i < this -> weights.size(); i++) {
+				// Grab the previous weight
+				float temp = this -> previous[i];
 				// Set the new rate using the gradient
-				this -> weights[i] = LEARN_WEIGHT * gradient(error, layer_output->at(i)) + MOMENTUM * temp;
+				this -> weights[i] = LEARN_RATE * gradient(error, this -> sigmoid(layer_output)) + MOMENTUM * temp;
+				// Save the difference as the new previous rate for the next calculation
+				this -> previous[i] = this -> weights[i] - this -> previous[i];
 			}
 		}
 	}
@@ -86,24 +87,24 @@ public:
 	virtual vector<float> get_layer_output() {
 		vector<float> ret = {};
 		// Get the outputs of the above layers recursively
-		vector<float> inputs = layer_above->get_layer_output();
+		vector<float> inputs = layer_above -> get_layer_output();
 		// Calculates the sigmoid of each input
 		for (int i = 0; i < neurons.size(); i++) {
-			ret.push_back(neurons[i]->sigmoid(&inputs));
+			ret.push_back(neurons[i] -> sigmoid(&inputs));
 		}
 		return ret;
 	}
 	//Does the whole backpropogation thing over a layer
 	virtual void backpropogate(float error) {
-		// This part does it for each neuron
+		// This part does it for each neuron in this layer
 		for (int i = 0; i < neurons.size(); i++) {
-			if (!neurons.at(i)->const_output) {
-				vector<float> output = layer_above->get_layer_output();
+			if (!neurons.at(i) -> const_output) {
+				vector<float> output = layer_above -> get_layer_output();
 				neurons.at(i)->backpropogate(error, &output);
 			}
 		}
-		// THis thing does backpropogation for all of it
-		layer_above->backpropogate(error);
+		// This thing does backpropogation for the layer above
+		layer_above -> backpropogate(error);
 	}
 	// This corrects the error
 	float correct_error(float error) {
@@ -128,7 +129,7 @@ public:
 		return inputs;
 	}
 	// This needs to be empty since you don't do it for backpropogation
-	void backpropogate(float error) {}
+	void backpropogate(float error) { }
 	// Constructor for the inputlayer
 	InputLayer(vector<float> outputs) {
 		inputs = outputs;
@@ -136,21 +137,21 @@ public:
 };
 
 int main() {
-	// Declare the neurons in the middle layer
+	// Create the neurons in the middle layer
 	vector<float> h1_weights = { rand_float(), rand_float(), rand_float() };
-	Neuron h1(0.5, h1_weights);
+	Neuron h1(0.5f, h1_weights);
 	vector<float> h2_weights = { rand_float(), rand_float(), rand_float() };
-	Neuron h2(0.5, h2_weights);
-	Neuron b2(true, 1.0);
+	Neuron h2(0.5f, h2_weights);
+	Neuron b2(true, 1.0f);
 
-	// Declare the neuron in the output layer
+	// Create the output neuron
 	vector<float> o1_weights = { rand_float(), rand_float(), rand_float() };
 	Neuron o1(0.5, o1_weights);
 
 	cout << "Neurons created!" << endl;
 
 	// Create the input layer
-	vector<float> input_outputs = { 0.0, 1.0, 1.0 }; // such a poetic name
+	vector<float> input_outputs = { 0.0f, 1.0f, 1.0f }; // such a poetic name
 	InputLayer input_layer(input_outputs);
 
 	// Create the middle layer and add neurons to it
@@ -169,7 +170,7 @@ int main() {
 
 	float error = output_layer.correct_error(1.0f - output_layer.get_layer_output()[0]);
 
-	while (error > 0.05) {
+	while (error > 0.05f) {
 		error = output_layer.correct_error(1.0f - output_layer.get_layer_output()[0]);
 		cout << error << endl;
 	}
